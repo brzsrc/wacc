ad7aa0931e49d8009a058296fa75fb025e47e239
徐世桐 <xushitong@xushitongdeMacBook-Pro.local>
2021-01-30 10:12:06 +0000

    sx119: src/Test.java: added example java file given in lecture; added 2 other example files

---
+++ src/test.java
@@ -0,0 +1,21 @@
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+import antlr.*;
+
+public class Test{
+  public static void main(String[] args) throws Exception {
+    CharStream input = CharStreams.fromStream(System.in);
+
+    FullLexer lexer = new FullLexer(input);
+
+    CommonTokenStream tokens  = new CommonTokenStream(lexer);
+
+    FullParser parser = new FullParser(tokens);
+
+    ParseTree tree = parser.expr();
+
+    System.out.println(tree.toStringTree(parser));
+
+  }
+}
