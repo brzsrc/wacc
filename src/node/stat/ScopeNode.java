870a3e56276865596b2aff4c5e4f712f1a0b9db5
xx1219 <xx1219@ic.ac.uk>
2021-02-09 07:07:44 -0800

    xx1219: merge SeqNode into ScopeNode

--- src/node/stat/ScopeNode.java
+++ src/node/stat/ScopeNode.java
@@ -1,23 +1,46 @@
 package node.stat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ScopeNode extends StatNode {
 
-  private final StatNode body;
+  private final List<StatNode> body = new ArrayList<>();
 
-  public ScopeNode(StatNode body) {
-    this.body = body;
+  public ScopeNode(StatNode node) {
+    mergeScope(node);
     setAll();
+  }
+
+  /* Handle the SeqStat */
+  public ScopeNode(StatNode before, StatNode after) {
+    mergeScope(before);
+    mergeScope(after);
+    setAll();
+  }
+
+  private void mergeScope(StatNode s) {
+    if (s instanceof ScopeNode) {
+      body.addAll(((ScopeNode) s).body);
+    } else {
+      body.add(s);
+    }
   }
 
   @Override
   public void setLeaveAtEnd() {
-    assert body != null;
-    leaveAtEnd = body.leaveAtEnd();
+    assert body.size() > 0;
+    leaveAtEnd = body.get(body.size()-1).leaveAtEnd();
   }
 
   @Override
   public void setHasReturn() {
-    assert body != null;
-    hasReturn = body.hasReturn();
+    for (StatNode node : body) {
+      if (node.hasReturn()) {
+        hasReturn = true;
+        return;
+      }
+    }
+    hasReturn = false;
   }
 }
