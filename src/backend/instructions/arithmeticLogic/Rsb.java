6faaa5e5919c0d82955f66958f2c1bac405d899d
Tom Zhao <zhaoxuan0914@hotmail.com>
2021-03-02 23:58:51 +0800

    xz1919: fixed all runtimeErr issues of inability to check over/underflow

--- src/backend/instructions/arithmeticLogic/Rsb.java
+++ src/backend/instructions/arithmeticLogic/Rsb.java
@@ -6,12 +6,16 @@ import utils.backend.Register;
 
 public class Rsb extends ArithmeticLogic {
 
+  public enum RsbMode { RSB, RSBS }
+  private RsbMode mode;
+
   public Rsb(Register rd, Register rn, Operand2 operand2) {
     super(rd, rn, operand2);
+    mode = RsbMode.RSBS;
   }
 
   @Override
   public String assemble() {
-    return "RSB " + Rd + ", " + Rn + ", " + operand2;
+    return mode + " " + Rd + ", " + Rn + ", " + operand2;
   }
 }
